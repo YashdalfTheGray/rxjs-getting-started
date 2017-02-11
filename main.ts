956c7416c81e6f85ed3d5eb89f075c1688c2e70c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-11 02:35:35 -0800

    added lazy loading to the fetch based observables

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/observable/fromEvent";
 import "rxjs/add/observable/fromPromise";
+import "rxjs/add/observable/defer";
 import "rxjs/add/operator/mergeMap";
 import "rxjs/add/operator/map";
 
@@ -12,7 +13,9 @@ const output = <HTMLElement>document.querySelector('#output');
 const button = <HTMLElement>document.querySelector('#button');
 
 const source = Observable.fromEvent(button, 'click');
-const load = (url: string) => Observable.fromPromise(fetch(url).then(r => r.json<Array<Movie>>()));
+const load = (url: string) => Observable.defer(() => Observable.fromPromise(fetch(url).then(r => r.json<Array<Movie>>())));
+
+// some retry logic - retryWhen(errors => errors.scan((acc, e) => acc + 1, 0).takeWhile(acc => acc < 4).delay(1000))
 
 source.flatMap(e => load('movies.json'))
 .map(movies => movies.map(m => `<div>${m.title}</div>`).join(''))
