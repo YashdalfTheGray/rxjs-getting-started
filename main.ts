6af4d388de21241bf1025b962a72be53d7b20ed3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-11 03:15:07 -0800

    added retry logic to the movie fetching'

--- main.ts
+++ main.ts
@@ -1,21 +1,12 @@
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/observable/fromEvent";
-import "rxjs/add/observable/fromPromise";
-import "rxjs/add/observable/defer";
-import "rxjs/add/operator/mergeMap";
-import "rxjs/add/operator/map";
 
-interface Movie {
-    title: string
-}
+import "./load-operators";
+import { load } from "./loader";
 
 const output = <HTMLElement>document.querySelector('#output');
 const button = <HTMLElement>document.querySelector('#button');
 
 const source = Observable.fromEvent(button, 'click');
-const load = (url: string) => Observable.defer(() => Observable.fromPromise(fetch(url).then(r => r.json<Array<Movie>>())));
-
-// some retry logic - retryWhen(errors => errors.scan((acc, e) => acc + 1, 0).takeWhile(acc => acc < 4).delay(1000))
 
 source.flatMap(e => load('movies.json'))
 .map(movies => movies.map(m => `<div>${m.title}</div>`).join(''))
