265f658c628f8cb729ab69a5358220b05205f0fe
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-11 03:14:16 -0800

    moved the operator and observable mixin loading to another file'

---
+++ load-operators.ts
@@ -0,0 +1,8 @@
+import "rxjs/add/observable/fromEvent";
+import "rxjs/add/observable/fromPromise";
+import "rxjs/add/observable/defer";
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/retryWhen";
+import "rxjs/add/operator/scan";
+import "rxjs/add/operator/delay";
