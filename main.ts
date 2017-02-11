b6276475e2947e7c214ad5d50441296be4ef1aee
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-11 00:25:44 -0800

    got a circle that tracks the mouse on a slight delay

--- main.ts
+++ main.ts
@@ -1,47 +1,20 @@
 import { Observable } from "rxjs/Observable";
-import { Observer } from "rxjs/Observer";
+import "rxjs/add/observable/fromEvent";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/filter";
+import "rxjs/add/operator/delay";
 
-const numbers: Array<Number> = [1, 5, 10, 15, 20];
+const circle = <HTMLElement>document.querySelector('#circle');
 
-class MyObserver implements Observer<Number> {
-    next(value) {
-        console.log(`value is ${value}`);
-    }
+const source = Observable.fromEvent(document, 'mousemove')
+.map((e: MouseEvent) => [e.clientX, e.clientY])
+.filter(v => v[0] < 500)
+.delay(250);
 
-    error(e) {
-        console.log(`error: ${e}`);
-    }
+const onNext = (value) => [circle.style.left, circle.style.top] = value.map(v => v + 'px');
 
-    complete() {
-        console.log('complete');
-    }
-}
-
-const source = Observable.create(observer => {
-    let index = 0;
-
-    const produceValues = () => {
-        observer.next(numbers[index++]);
-
-        if (index < numbers.length) {
-            setTimeout(produceValues, 250);
-        }
-        else {
-            observer.complete();
-        }
-    };
-
-    produceValues();
-})
-.map(n => n * 2)
-.filter(n => n > 4);
-
-
-source.subscribe(new MyObserver());
 source.subscribe(
-    v => console.log(`Got value: ${v}`),
+    onNext,
     e => console.log(`Got error: ${e}`),
     () => console.log('No more stuff left')
 );
